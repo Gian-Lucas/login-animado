c659ade9a669c81c51b09febf0631bc304e6f77b
= <=>
2021-02-28 19:51:53 -0300

    complete

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
-let labelLogin = document.querySelector('.l1');
-let inputLogin = document.getElementById('user');
-let labelPassword = document.querySelector('.l2');
-let inputPassword = document.getElementById('pass');
+const [labelUsername, labelPassword] = document.querySelectorAll('.label');
 
-inputLogin.addEventListener('focus', () => {
-    labelLogin.classList.add('up1');
-});
-inputLogin.addEventListener('blur', () => {
-    if (inputLogin.value == '') {
-        labelLogin.classList.remove('up1');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', e => {
+
+    if (verifyFields(username, password)) {
+        alert('Login');
+    } else {
+        e.preventDefault();
+        alert('Preencha os campos, por favor');
+    }
+})
+
+username.addEventListener('focus', () => {
+    if (!verifyFields(username)) {
+        labelUsername.classList.add('on');
     }
 });
 
-inputPassword.addEventListener('focus', () => {
-    labelPassword.classList.add('up2');
-});
-inputPassword.addEventListener('blur', () => {
-    if (inputPassword.value == '') {
-        labelPassword.classList.remove('up2');
+username.addEventListener('blur', () => {
+    if (!verifyFields(username)) {
+        labelUsername.classList.remove('on');
     }
 });
+
+
+password.addEventListener('focus', () => {
+    if (!verifyFields(password)) {
+        labelPassword.classList.add('on');
+    }
+});
+
+password.addEventListener('blur', () => {
+    if (!verifyFields(password)) {
+        labelPassword.classList.remove('on');
+    }
+});
+
+function verifyFields(...fields) {
+    let isValid = true;
+
+    fields.forEach(field => {
+        if (field.value.trim() === '') {
+            isValid = false;
+        }
+    });
+
+    return isValid;
+}
